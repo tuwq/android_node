3e77116d17ea6ae632da799e1013c9d78624c79a
tuwq <aheadqiang@126.com>
2019-07-03 21:16:06 +0800

    安装包更新版本

---
+++ demo/mobilesafe/app/src/main/java/com/tuwq/mobilesafe/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.tuwq.mobilesafe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
